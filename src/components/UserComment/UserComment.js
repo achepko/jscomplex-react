8a35de215b11ded2ce90c9962006d7024060747e
Anton Chepko <choptonys@gmail.com>
2023-04-17 23:17:07 +0200

    17.04.23 - homework 3 (cars,users with validators,comments)

--- src/components/UserComment/UserComment.js
+++ src/components/UserComment/UserComment.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
+import userService from "../../services/user.service";
 
 const UserComment = () => {
+
+    const {register,handleSubmit,reset} = useForm();
+
+    const onSubmitComment = async (comment)=>{
+        const {data} = await userService.createComment(comment);
+        console.log("New comment created:", data);
+        reset()
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit(onSubmitComment)}>
+            <input type="text" placeholder={'name'} {...register('name', { required: true})} />
+            <input type="text" placeholder={'email'} {...register('email', {required: true})}/>
+            <input type="text" placeholder={'body'} {...register('body', {required: true})}/>
+            <button>Add new comment</button>
+        </form>
     );
 };
 
