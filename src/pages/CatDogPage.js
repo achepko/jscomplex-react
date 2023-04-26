b33bbe45b34701650f2771180e9ba264cbba29e0
Anton Chepko <choptonys@gmail.com>
2023-04-26 23:51:31 +0200

    26.04.2023 - homework 5 (final) to be updated later

--- src/pages/CatDogPage.js
+++ src/pages/CatDogPage.js
@@ -1,9 +1,39 @@
-import React from 'react';
-
+import React, {useReducer} from 'react';
+import Cats from "../components/Cats/Cats";
+import Dogs from "../components/Dogs/Dogs";
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_CAT':
+            const cat = action.payload;
+            const catId = state.cats.slice(-1)[0]?.id + 1 || 1;
+            cat.id = catId;
+            return {...state, cats: [...state.cats, cat]}
+        case 'ADD_DOG':
+            const dog = action.payload;
+            const dogId = state.dogs.slice(-1)[0]?.id + 1 || 1;
+            dog.id = dogId;
+            return {...state, dogs: [...state.dogs, dog]}
+        case 'DEL_CAT':
+            const idForDeleteCat = action.payload;
+            const catIndex = state.cats.findIndex(value => value.id === idForDeleteCat);
+            state.cats.splice(catIndex, 1);
+            return {...state}
+        case 'DEL_DOG':
+            const idForDeleteDog = action.payload;
+            const dogIndex = state.dogs.findIndex(value => value.id === idForDeleteDog);
+            state.dogs.splice(dogIndex, 1);
+            return {...state}
+        default:
+            return state;
+    }
+}
 const CatDogPage = () => {
+
+    const stateDispatch = useReducer(reducer, {cats: [], dogs: []});
     return (
-        <div>
-            Cats and dogs
+        <div style={{display: 'flex', justifyContent: "space-around", marginTop: '50px'}}>
+            <Cats stateDispatch={stateDispatch}/>
+            <Dogs stateDispatch={stateDispatch}/>
         </div>
     );
 };
